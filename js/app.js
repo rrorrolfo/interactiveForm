a38312fda584be3d4139427c551d5d06cda1d593
Rodolfo Perez <rodolfobeste@gmail.com>
2018-11-18 01:31:28 +0200

    Added functionality for job role and t-shirt fields

---
+++ js/app.js
@@ -0,0 +1,66 @@
+
+// Focus name input when page is loaded
+const $name = $("#name");
+
+$(document).ready(function() {
+    $name.focus();
+})
+
+//Hides extra input for job role 
+const $job_role = $("#other-title");
+
+$job_role.hide();
+
+
+/////////// T-SHIRT INFO////////////////7
+
+// hides color select menu untill a design is chosen
+
+const $design = $("#design");
+const $shirt_color = $("#colors-js-puns");
+const $color = $("#color");
+const $color_options = $("#color option");
+
+$shirt_color.hide();
+
+// Event listener for displaying color options after user selects the design on the T-shirt
+
+$design.on("change", () => {
+
+    //Resets value in select input
+    $color.val("")
+
+    //Displays options for "JS puns"
+    if ($design.val() === "js puns") {
+        $shirt_color.show();
+        
+        $color_options.each((index, element) => {
+
+            $(element).hide();
+
+            if (index <= 2) {
+                $(element).show();
+            } 
+
+        })
+
+    //Displays options for"I JS"
+    } else if ($design.val() === "heart js") {
+        $shirt_color.show();
+
+        $color_options.each((index, element) => {
+
+            $(element).hide();
+
+            if (index >= 3) {
+                $(element).show();
+            } 
+
+        })
+
+        //hides color select input if neither of two options is selected
+    } else {
+        $shirt_color.hide();
+    }
+});
+
